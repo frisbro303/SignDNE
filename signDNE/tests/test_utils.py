c5219f6513fc5fc27ccef88bb5fb2e31ac9770f6
wush303 <felix@risbro.dk>
2025-04-05 12:13:23 +0200

    Closeholes test

--- signDNE/tests/test_utils.py
+++ signDNE/tests/test_utils.py
@@ -3,7 +3,7 @@ import trimesh
 from utils import close_holes
 
 def test_close_holes():
-    mesh = trimesh.load("../data/normal.ply")
+    mesh = trimesh.load("signDNE/normal.ply")
     watertight_mesh = close_holes(mesh)
     assert mesh.is_watertight()
 
