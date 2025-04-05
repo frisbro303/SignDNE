3efafc75e43f8e7228a63391eb2a6bef34510235
wush303 <felix@risbro.dk>
2025-04-05 12:10:51 +0200

    Added simple test

--- signDNE/tests/test_utils.py
+++ signDNE/tests/test_utils.py
@@ -1,2 +1,9 @@
 import pytest
+import trimesh
+from utils import close_holes
+
+def test_close_holes():
+    mesh = trimesh.load("../data/normal.ply")
+    watertight_mesh = close_holes(mesh)
+    assert mesh.is_watertight()
 
